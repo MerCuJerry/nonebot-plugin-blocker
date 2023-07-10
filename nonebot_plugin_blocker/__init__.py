21c761ca5eb1558197fc685c7e07726147b3ddb5
MerCuJerry <a1159294481@gmail.com>
2023-07-11 01:00:52 +0800

    解决插件元数据问题

--- nonebot_plugin_blocker/__init__.py
+++ nonebot_plugin_blocker/__init__.py
@@ -2,10 +2,11 @@ from nonebot.plugin import PluginMetadata
 
 from .__main__ import *  # noqa
 
-__version__ = "0.1.5"
+__version__ = "0.1.6"
 __plugin_meta__ = PluginMetadata(
     name="Blocker",
     description='分群配置关闭Bot插件',
+    usage='使用指令阻止机器人在该群回复',
     type="application",
     homepage="https://github.com/MerCuJerry/nonebot-plugin-blocker",
     supported_adapters={"~onebot.v11"},
