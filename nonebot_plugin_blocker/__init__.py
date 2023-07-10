bcb7b2769e4c4a089d65b13f57ca29b24d84a38a
MerCuJerry <a1159294481@gmail.com>
2023-07-11 00:53:06 +0800

    modified:   nonebot_plugin_blocker/__init__.py 	modified:   pyproject.toml

--- nonebot_plugin_blocker/__init__.py
+++ nonebot_plugin_blocker/__init__.py
@@ -2,9 +2,11 @@ from nonebot.plugin import PluginMetadata
 
 from .__main__ import *  # noqa
 
-__version__ = "0.1.4"
+__version__ = "0.1.5"
 __plugin_meta__ = PluginMetadata(
-    "Blocker",
-    "分群配置关闭所有骰子回复插件",
-    "虽然我觉得关闭骰子直接飞出去就好了但是还是有人要来问能不能做这个插件所以我就做了",
+    name="Blocker",
+    description='分群配置关闭Bot插件',
+    type="application",
+    homepage="https://github.com/MerCuJerry/nonebot-plugin-blocker",
+    supported_adapters={"~onebot.v11"},
 )
